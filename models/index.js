e41ca3c5c851919dcfe6f29cf90fc0b11b58de6b
Venus DM <crhowe90@gmail.com>
2022-08-27 10:35:35 -0500

    associations

--- models/index.js
+++ models/index.js
@@ -1,3 +1,33 @@
 const Comment = require('./Comment');
 const Post = require('./Post');
 const User = require('./User');
+
+//a user can have many posts
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+});
+
+//A post can only belong to one user
+Post.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+//a comment can only belong to one user
+Comment.belongsTo(User,{
+    foreignKey: 'user_id'
+});
+
+//a comment will belong to single post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+//a user can have many comments
+User.hasMany(Comment,{
+    foreignKey: 'user_id'
+});
+
+//there can be many comments on one post
+Post.hasMany(Comment,{
+    foreignKey: 'post_id'
+});
