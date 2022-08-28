e16e34f6a9b0382047fc4ceff1132ed4335bc3b6
Venus DM <crhowe90@gmail.com>
2022-08-28 18:36:18 -0500

    comment routes working

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+//get all comments
+router.get('/', (req, res) => {
+    Comment.findAll({
+
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err);
+        });
+});
+
+//create a comment
+router.post('/', (req, res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        user_id: req.body.user_id
+    })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//update a comment
+router.put('/:', (req, res) => {
+    Comment.update({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'No comment found with this id' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//delete a comment
+router.delete('/:id', (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(400).json({ message: "No comment with this id found!" });
+                return;
+            }
+            req.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
