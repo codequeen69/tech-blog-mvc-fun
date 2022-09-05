a7fdca99422cf90ddd3d91754cc2c36833c2d6ae
Venus DM <crhowe90@gmail.com>
2022-09-05 09:26:49 -0500

    delete truncate

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection')
 const { Post, User, Comment }= require('../models');
-const { truncate } = require('../models/Comment');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
